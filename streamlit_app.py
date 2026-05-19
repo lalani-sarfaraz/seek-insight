a7bd2073ea24205aa169a9ffbb78d74ac6f55265
lalani-sarfaraz <34720929+lalani-sarfaraz@users.noreply.github.com>
2026-05-19 02:19:15 +0000

    Added Volcano plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ if uploaded_file:
     st.write(df.head())
 
 
+# Basic QC checks:
 
 sig = df[df["padj"] < 0.05]
 
@@ -24,3 +25,23 @@ st.write(f"Total genes: {len(df)}")
 st.write(f"Significant genes: {len(sig)}")
 st.write(f"Upregulated: {len(up)}")
 st.write(f"Downregulated: {len(down)}")
+
+
+
+
+# Volcano plots:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+df["neglog10_padj"] = -np.log10(df["padj"])
+
+fig, ax = plt.subplots()
+
+ax.scatter(
+    df["log2FoldChange"],
+    df["neglog10_padj"],
+    s=5
+)
+
+st.pyplot(fig)

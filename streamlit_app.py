ff01d6062524be118e300ddb2220e2d1d7c0cfac
lalani-sarfaraz <34720929+lalani-sarfaraz@users.noreply.github.com>
2026-05-24 04:26:10 +0000

    Gemini 2.0 flash

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # -----------------------------
 # FILE UPLOAD
